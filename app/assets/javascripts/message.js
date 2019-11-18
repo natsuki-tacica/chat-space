c38613408ee09b6e71b9da00b913ba6a4f3fd603
natsuki-tacica <sns.senyou.n@gmail.com>
2019-11-18 16:18:07 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,44 @@
 $(function(){
   function buildHTML(message){
     if ( message.image ){
-    var html =
-     `<div class="message" data-message-id=${message.id}>
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-          ${message.user_name}
-        </div>
-        <div class="upper-message__date">
-          ${message.data}
-        </div>
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.data}
+            </div>
+            </div>
+            <div class="lower-message">
+              <p class="lower-message__content">
+              ${message.content}
+            </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+      return html;
+    }　else　{
+      var html =
+      `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.data}
+          </div>
         </div>
         <div class="lower-message">
           <p class="lower-message__content">
-          ${message.content}
-        </p>
+            ${message.content}
+          </p>
         </div>
-        <img src=${message.image} >
-      </div>`
+      </div>` 
     return html;
-  }else{
-    var html =
-    `<div class="message" data-message-id=${message.id}>
-      <div class="upper-message">
-        <div class="upper-message__user-name">
-        ${message.user_name}
-      </div>
-      <div class="upper-message__date">
-        ${message.data}
-      </div>
-      </div>
-      <div class="lower-message">
-        <p class="lower-message__content">
-          ${message.content}
-        </p>
-      </div>
-    </div>` 
-  return html;
-  };
-}
-
+    };
+  }
 $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -55,10 +54,9 @@ $('#new_message').on('submit', function(e){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('#message_content').val('');
+      $('form')[0].reset();
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
       $('.form__submit').prop('disabled', false);
-
     })
     .fail(function(){
       alert('error');
