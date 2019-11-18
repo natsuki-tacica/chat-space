8810ecdcc9d9bac72a0785d5eeaf51bafbf55260
natsuki-tacica <sns.senyou.n@gmail.com>
2019-11-18 17:05:37 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,24 @@
 $(function(){
   function buildHTML(message){
-    if ( message.image ){
-      var html =
+    var image = message.image ? `<img class="lower-message__image" src="${message.image}" >` : "";
+    var html =
         `<div class="message" data-message-id=${message.id}>
             <div class="upper-message">
               <div class="upper-message__user-name">
               ${message.user_name}
             </div>
-            <div class="upper-message__date">
-              ${message.data}
+              <div class="upper-message__date">
+              ${message.date}
             </div>
             </div>
             <div class="lower-message">
               <p class="lower-message__content">
               ${message.content}
-            </p>
+              </p>
             </div>
-            <img src=${message.image} >
           </div>`
       return html;
-    }　else　{
-      var html =
-      `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.data}
-          </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        </div>
-      </div>` 
-    return html;
-    };
-  }
+  };   
 $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -64,3 +44,4 @@ $('#new_message').on('submit', function(e){
     return false;
   });
 });
+
